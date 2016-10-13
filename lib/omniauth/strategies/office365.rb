627fd7ce7d70180e23801201afdeae511546c21e
Tommy Bonderenka <machine.valley@gmail.com>
2016-10-13 15:40:43 -0500

    fix

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -7,12 +7,8 @@ module OmniAuth
 
       option :client_options, {
         site:          'https://login.microsoftonline.com/',
-        token_url:     'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-        authorize_url: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
-      }
-
-      option :authorize_params, {
-        resource: 'https://graph.windows.net/'
+        token_url:     'https://login.microsoftonline.com/415abcba-efe6-43bc-b3e7-1f1e9fb2dd3b/oauth2/v2.0/token',
+        authorize_url: 'https://login.microsoftonline.com/415abcba-efe6-43bc-b3e7-1f1e9fb2dd3b/oauth2/v2.0/authorize'
       }
 
       uid { raw_info["objectId"] }
@@ -32,7 +28,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get(authorize_params.resource + 'Me?api-version=1.5').parsed
+        @raw_info ||= access_token.get("https://outlook.office.com/api/v2.0/me/").parsed
       end
     end
   end

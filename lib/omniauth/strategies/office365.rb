d906430f69900eddef88407fd02df7b347778422
Tommy Bonderenka <machine.valley@gmail.com>
2016-10-13 15:45:55 -0500

    fix slitjg

--- lib/omniauth/strategies/office365.rb
+++ lib/omniauth/strategies/office365.rb
@@ -4,12 +4,16 @@ module OmniAuth
   module Strategies
     class Office365 < OmniAuth::Strategies::OAuth2
       option :name, :office365
+      
+      DEFAULT_SCOPE="openid email profile https://outlook.office.com/contacts.read"
 
       option :client_options, {
         site:          'https://login.microsoftonline.com/',
         token_url:     'https://login.microsoftonline.com/415abcba-efe6-43bc-b3e7-1f1e9fb2dd3b/oauth2/v2.0/token',
         authorize_url: 'https://login.microsoftonline.com/415abcba-efe6-43bc-b3e7-1f1e9fb2dd3b/oauth2/v2.0/authorize'
       }
+      
+      option :authorize_options, [:scope]
 
       uid { raw_info["objectId"] }
 
@@ -29,6 +33,18 @@ module OmniAuth
 
       def raw_info
         @raw_info ||= access_token.get("https://outlook.office.com/api/v2.0/me/").parsed
+      end
+      
+      def authorize_params
+        super.tap do |params|
+          %w[display scope auth_type].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
       end
     end
   end
